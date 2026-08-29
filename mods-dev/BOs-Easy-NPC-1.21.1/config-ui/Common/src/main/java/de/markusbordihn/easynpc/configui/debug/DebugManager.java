c181b5739a9b7ef8820a05726d7db405b9cabbf4
Yanlai Wu <76715801+CatfishW@users.noreply.github.com>
2026-08-28 23:41:26 -0400

    fix(easy-npc): restore Config UI debug service

---
+++ mods-dev/BOs-Easy-NPC-1.21.1/config-ui/Common/src/main/java/de/markusbordihn/easynpc/configui/debug/DebugManager.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2023 Markus Bordihn
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package de.markusbordihn.easynpc.configui.debug;
+
+import de.markusbordihn.easynpc.configui.Constants;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.config.Configuration;
+import org.apache.logging.log4j.core.config.LoggerConfig;
+
+public final class DebugManager {
+
+  private static final Logger log = LogManager.getLogger(Constants.LOG_NAME);
+  private static final String LOG_PREFIX = "[Debug Manager]";
+  private static boolean developmentEnvironment;
+
+  private DebugManager() {}
+
+  public static void setLogLevel(Logger logger, Level logLevel) {
+    if (logger == null || logLevel == null || logLevel == logger.getLevel()) {
+      return;
+    }
+
+    String loggerName = logger.getName();
+    LoggerContext context = (LoggerContext) LogManager.getContext(false);
+    Configuration configuration = context.getConfiguration();
+    LoggerConfig inheritedConfig = configuration.getLoggerConfig(loggerName);
+    LoggerConfig targetConfig = inheritedConfig;
+
+    if (!inheritedConfig.getName().equals(loggerName)) {
+      log.info("{} Add logger config for {} with level {}.", LOG_PREFIX, loggerName, logLevel);
+      targetConfig = new LoggerConfig(loggerName, logLevel, true);
+      targetConfig.setParent(inheritedConfig);
+      configuration.addLogger(loggerName, targetConfig);
+    } else {
+      log.info(
+          "{} Change log level for {} from {} to {}.",
+          LOG_PREFIX,
+          loggerName,
+          logger.getLevel(),
+          logLevel);
+    }
+
+    targetConfig.setLevel(logLevel);
+    context.updateLoggers();
+  }
+
+  public static void enableDebugLevel(boolean enable) {
+    enableDebugLevel(Constants.LOG_NAME, enable);
+  }
+
+  public static void enableDebugLevel(String loggerName, boolean enable) {
+    setLogLevel(LogManager.getLogger(loggerName), enable ? Level.DEBUG : Level.INFO);
+  }
+
+  public static Level getLogLevel(String loggerName) {
+    return LogManager.getLogger(loggerName).getLevel();
+  }
+
+  public static boolean isDebugLevel(String loggerName) {
+    Level level = getLogLevel(loggerName);
+    return level == Level.DEBUG || level == Level.TRACE || level == Level.ALL;
+  }
+
+  public static void checkForDebugLogging(String loggerName) {
+    if (!isDebugLevel(loggerName)) {
+      return;
+    }
+
+    Logger logger = LogManager.getLogger(loggerName);
+    String logLevelName = logger.getLevel().name();
+    logger.warn("{} Detected debug log level for {} with {}!", LOG_PREFIX, loggerName, logLevelName);
+
+    if (isDevelopmentEnvironment()) {
+      logger.info("{} Development environment detected; keeping {} at {}.", LOG_PREFIX, loggerName, logLevelName);
+      return;
+    }
+
+    logger.warn(
+        "{} Adjusting log level for {} from {} to {} for performance.",
+        LOG_PREFIX,
+        loggerName,
+        logLevelName,
+        Level.INFO);
+    enableDebugLevel(loggerName, false);
+  }
+
+  public static boolean isDevelopmentEnvironment() {
+    return developmentEnvironment;
+  }
+
+  public static void setDevelopmentEnvironment(boolean developmentEnvironment) {
+    DebugManager.developmentEnvironment = developmentEnvironment;
+  }
+
+  public static boolean isDebugMode() {
+    return isDevelopmentEnvironment() || isDebugLevel(Constants.LOG_NAME);
+  }
+}
